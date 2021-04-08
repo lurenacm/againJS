d3fc36e554517140f566f1d3029c52375837075d
lurenacm <18320398437@139.com>
2021-04-08 20:12:24 +0800

    end ajax

--- js/原生Ajax/promise-ajax.js
+++ js/原生Ajax/promise-ajax.js
@@ -1,7 +1,40 @@
 (function () {
-    let promiseAjax = function promiseAjax() {
-
+    // 配置默认属性
+    let defaultConfigs = {
+        url: null,
+        baseUrl: null,
+        method: 'GET',
+        body: null,
+        dataType: 'JSON',
+        params: null,   // get 请求中的 ? 传参
+        headers: {},
+        cache: false    // catch用于缓存，catch 在 axios 中没有。
     }
+
+    let promiseAjax = function promiseAjax(optionsConfigs={}) {
+
+        let {url, method ='GET', baseUrl, body, dataType, params, headers, cache} = optionsConfigs
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest()
+            xhr.open(method, url=baseUrl+url)
+            // 判断和设置请求头
+            if(headers !== null && typeof headers == 'object'){
+                
+            }
+            xhr.send(body)
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    dataType = dataType.toUpperCase()
+                    let res = dataType === 'JSON' ? JSON.parse(xhr.responseText) : (dataType === 'xml' ? xhr.responseXML : null)
+                    resolve(res, xhr)
+                }
+                reject(xhr.statusText, xhr)
+            }
+        })
+    }
+
+    // 提供外部的默认配置选项
+    promiseAjax.defaultConfigs = defaultConfigs
 
     promiseAjax.get = function (url, config) {
 
@@ -14,5 +47,7 @@
     promiseAjax.head = function () {
 
     }
+
+    window.promiseAjax = promiseAjax
     
 })(window)
--- js/原生Ajax/promise-ajax.md
+++ js/原生Ajax/promise-ajax.md
@@ -1,6 +1,6 @@
 ## 使用 promise 封装 Ajax 实现一个简单版的 axios。
 ### 目标：实现 `get/post/head` 的方法。
-* 将封装的函数挂载到 `window` 上，并没有封装成一个 `class`
+* 将封装的函数挂载到 `window` 上，这样外部可以修改某些属性。这里没有封装成一个 `class`。
 * `get/post/head` 都有两个相同的参数，`url,config`，post 还有一个 data 参数对象
 * 
 ``` js
