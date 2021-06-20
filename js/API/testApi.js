de4db77d01ce1675faf528a694cecd0e8839484f
“lurenacm” <18320398437@139.com>
2021-06-20 22:10:27 +0800

    end 模拟实现 new

--- js/API/testApi.js
+++ js/API/testApi.js
@@ -182,9 +182,17 @@ Dog.prototype.sayName = function() {
     console.log('my name is ' + this.name)
 }
 
-function _new() {
-    // code
-    return new arguments[0](arguments[1])
+function _new(ctor, ...params) {
+    // 创建一个堆内存地址，继承原型上的共有属性
+    let obj = {}
+    obj.__proto__ = ctor.prototype
+
+    // 确定 this 指向堆内存地址，同时使用 call 将构造函数的私有属性指向到 obj 实例中，实现私有属性继承
+    let res = ctor.call(obj, ...params)
+
+    // 返回创建的实例，考虑到构造函数本身执行后返回值是对象的话会覆盖返回的实例，需要先判断
+    if(res !== null && typeof res === 'object') return res
+    return obj
 }
 
 let sanmao = _new(Dog, '三毛')
